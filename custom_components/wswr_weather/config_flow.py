611c041ba1c8a19ec72c9793397af1e886e40d1b
Ge Hagen <ge@enlightensystems.co.nz>
2025-03-15 12:47:05 +1300

    Add descriptions to config_flow

--- custom_components/wswr_weather/config_flow.py
+++ custom_components/wswr_weather/config_flow.py
@@ -4,8 +4,8 @@ from homeassistant import config_entries
 from .const import CONF_API_URL, DOMAIN, CONF_INTERVAL
 
 DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_API_URL, default="https://api.wswr.jkent.tech/weatherdata/mostrecent/60"): str,
-    vol.Required(CONF_INTERVAL, default=1): int
+    vol.Required("api_url", default="https://api.wswr.jkent.tech/weatherdata/mostrecent/60"): str,
+    vol.Required("interval", default=1): int
 })
 
 class WeatherStationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -20,5 +20,15 @@ class WeatherStationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=DATA_SCHEMA,
-            errors={}
+            errors={},
+            description=(
+                "Please configure the WSWR Weather Station integration by providing the following details:\n\n"
+                "- **API URL:** Enter the URL of your weather data endpoint. "
+                "The default value points to the most recent weather data.\n"
+                "- **Interval:** Set the frequency (in minutes) at which the data should be fetched."
+            ),
+            description_placeholders={
+                "api_url_help": "Your API endpoint URL",
+                "interval_help": "Update frequency in minutes",
+            }
         )
