12792168b22116bdb84b3e39222db3da2969cd5a
Ge Hagen <ge@enlightensystems.co.nz>
2025-03-15 15:24:50 +1300

    try setup_entry again

--- custom_components/wswr_weather/__init__.py
+++ custom_components/wswr_weather/__init__.py
@@ -15,3 +15,25 @@ async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     _LOGGER.info("WSWR Weather Station - async_setup")
 
     return True
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Set up Weather Station sensor entities from a config entry."""
+    _LOGGER.info("WSWR Weather Station - async_setup_entry")
+
+    _LOGGER.info("WSWR Weather Station - Creating coordinator")
+    coordinator = WeatherStationCoordinator(hass)
+
+    _LOGGER.info("WSWR Weather Station - refresh_data")
+    await coordinator.async_config_entry_first_refresh()
+
+    # Create one sensor per key in the latest JSON object.
+    sensors = [
+        WeatherStationSensor(coordinator, sensor_key)
+        for sensor_key in coordinator.data.keys()
+    ]
+
+    _LOGGER.debug("WSWR Weather Station - Creating Sensors", sensors)
+
+    # async_add_entities(sensors, update_before_add=True)
+
