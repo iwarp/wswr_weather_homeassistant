f777db6da381af8f884016f21e1c4e665b9c569f
Ge Hagen <ge@enlightensystems.co.nz>
2025-03-15 16:58:56 +1300

    Move to async_setup_platform

--- custom_components/wswr_weather/__init__.py
+++ custom_components/wswr_weather/__init__.py
@@ -1,41 +1,41 @@
 """WSWR.co.nz  Weather Station integration."""
 
-import logging
-from custom_components.wswr_weather.sensor import WeatherStationCoordinator, WeatherStationSensor
+# import logging
+# from custom_components.wswr_weather.sensor import WeatherStationCoordinator, WeatherStationSensor
 
-from homeassistant import core
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+# from homeassistant import core
+# from homeassistant.config_entries import ConfigEntry
+# from homeassistant.core import HomeAssistant
+# from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-_LOGGER = logging.getLogger(__name__)
+# _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Set up the WSWR Weather Station component."""
-    _LOGGER.info("WSWR Weather Station - async_setup")
+# async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+#     """Set up the WSWR Weather Station component."""
+#     _LOGGER.info("WSWR Weather Station - async_setup")
 
-    return True
+#     return True
 
-async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Set up Weather Station sensor entities from a config entry."""
-    _LOGGER.info("WSWR Weather Station - async_setup_entry")
+# async def async_setup_entry(
+#     hass: HomeAssistant, entry: ConfigEntry) -> None:
+#     """Set up Weather Station sensor entities from a config entry."""
+#     _LOGGER.info("WSWR Weather Station - async_setup_entry")
 
-    # _LOGGER.info("WSWR Weather Station - Creating coordinator")
-    # coordinator = WeatherStationCoordinator(hass)
+#     # _LOGGER.info("WSWR Weather Station - Creating coordinator")
+#     # coordinator = WeatherStationCoordinator(hass)
 
-    # _LOGGER.info("WSWR Weather Station - refresh_data")
-    # await coordinator.async_config_entry_first_refresh()
+#     # _LOGGER.info("WSWR Weather Station - refresh_data")
+#     # await coordinator.async_config_entry_first_refresh()
 
-    # # Create one sensor per key in the latest JSON object.
-    # sensors = [
-    #     WeatherStationSensor(coordinator, sensor_key)
-    #     for sensor_key in coordinator.data.keys()
-    # ]
+#     # # Create one sensor per key in the latest JSON object.
+#     # sensors = [
+#     #     WeatherStationSensor(coordinator, sensor_key)
+#     #     for sensor_key in coordinator.data.keys()
+#     # ]
 
-    # _LOGGER.debug("WSWR Weather Station - Creating Sensors", sensors)
+#     # _LOGGER.debug("WSWR Weather Station - Creating Sensors", sensors)
 
-    # # async_add_entities(sensors, update_before_add=True)
+#     # # async_add_entities(sensors, update_before_add=True)
 
-    return True
+#     return True
 
